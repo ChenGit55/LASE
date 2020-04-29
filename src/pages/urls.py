3d465376510957b488c3bedbccb2466e29737fc1
Chen <chenpsilva@gmail.com>
2020-04-29 20:06:17 -0300

    pages templates

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import Home
+from .views import Home, LionCubs, Evolution, Lions
 
 app_name = 'pages'
 
 urlpatterns = [
     path('', Home.as_view(), name='index'),
+    path('programs/lion-cubs/', LionCubs.as_view(), name='lion-cubs'),
+    path('programs/evolution/', Evolution.as_view(), name='evolution'),
+    path('programs/lions/', Lions.as_view(), name='lions'),
 ]
--- src/pages/views.py
+++ src/pages/views.py
@@ -6,3 +6,15 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView):
     template_name = 'index.html'
+
+
+class LionCubs(TemplateView):
+    template_name = 'lion-cubs.html'
+
+
+class Evolution(TemplateView):
+    template_name = 'evolution.html'
+
+
+class Lions(TemplateView):
+    template_name = 'lions.html'
